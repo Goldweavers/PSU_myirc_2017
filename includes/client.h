9e99a0c0eb08ba57749fee61de6ee8f7413db481
Corentin Le Coz-Podeur <corentin.le-coz-podeur@epitech.eu>
2018-05-27 16:11:04 +0200

    [update] client include

--- includes/client.h
+++ includes/client.h
@@ -12,11 +12,41 @@
 
 /* Commands */
 
-int msg(char *parameters[]);
+int msg(char *parameters[], socket_t server);
 
-int server(char *parameters[]);
+int join(char *parameters[], socket_t server);
+
+int server(char *parameters[], socket_t server);
+
+int part(char *parameters[], socket_t server);
+
+int nick(char *parameters[], socket_t server);
+
+int list(char *parameters[], socket_t server);
+
+int users(char *parameters[], socket_t server);
+
+int names(char *parameters[], socket_t server);
+
+int quit(char *parameters[], socket_t server);
+
+int accept_file(char *parameters[], socket_t server);
+
 
 /* !Commands */
+
+typedef struct cinfo {
+	char *nickname;
+	node_t *channels;
+	socket_t socket;
+
+}cinfo_t;
+
+/* commands.c */
+
+char *implode(char **parameters);
+
+/* !commands.c */
 
 bool find_client_commands(char *name, command_ptr *function);
 
